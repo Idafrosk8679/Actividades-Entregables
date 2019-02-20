21320e8e15a89a4ed600854008892ae98342e207
Aitor <aitor.1999@hotmail.com>
2019-02-20 10:29:45 +0100

    Actividad 6a de Sistemas

---
+++ Sistemas Informaticos/Actividad 6a/shellscripts3.sh
@@ -0,0 +1,25 @@
+read -p "Escribe tu nota: " x
+
+#while [ $x -lt 0 ]; do
+#    read -p "Escribe un número entre 0 y 10: " x
+#done
+
+#while [ $x -gt 10 ]; do
+#    read -p "Escribe un número entre 0 y 10: " x
+#done
+
+while [ $x -gt 10 ] || [ $x -lt 0 ]; do
+    read -p "Escribe un número entre 0 y 10: " x
+done
+
+if [ $x -lt 5 ]; then
+    echo "Suspendido"
+elif [ $x -lt 6 ]; then
+    echo "Aprobado"
+elif [ $x -lt 7 ]; then
+    echo "Bien"
+elif [ $x -lt 9 ]; then
+    echo "Notable"
+else
+    echo "Sobresaliente"
+fi
